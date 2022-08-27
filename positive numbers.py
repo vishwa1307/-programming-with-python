09fb512533dc7402b47e244da1ae5724441545fc
vishwa1307 <105576903+vishwa1307@users.noreply.github.com>
2022-08-27 09:20:59 +0530

    Add files via upload
    printing all positive numbers in range program solved by vishwanathan k

---
+++ positive numbers.py
@@ -0,0 +1,13 @@
+a=[12,-7,5,4,-14]
+list1=a
+print(list1)
+for number in a:
+    if number>=0:
+        print(number,end = " ")
+b=[12,14,-95,3,-87]
+print("\n")
+list2=b
+print(list2)
+for number in b:
+    if number>=0:
+        print(number,end = " ")
