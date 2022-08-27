32979f329013ba71b6acb85c1042a9f41598dce8
vishwa1307 <105576903+vishwa1307@users.noreply.github.com>
2022-08-27 09:43:26 +0530

    Add files via upload
    by vishwanathan k

---
+++ decreasing order of frequencies using dictionary.py
@@ -0,0 +1,10 @@
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+a=str(input("ENTER A STRING:"))
+print (most_frequent(a))
